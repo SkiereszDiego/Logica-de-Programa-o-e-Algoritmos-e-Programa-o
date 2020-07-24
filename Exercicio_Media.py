2ba4b35710698747fded684b387cb7cf4bbd2077
DiegoSkieresz <46655332+SkiereszDiego@users.noreply.github.com>
2020-07-24 15:28:20 -0300

    Add files via upload

---
+++ Exercicio_Media.py
@@ -0,0 +1,9 @@
+'''4.	Ler três valores numéricos e escrever a média aritmética.'''
+
+a = float(input("digite um numero:"))
+b = float(input("digite outro numero:"))
+c = float(input("digite outro numero:"))
+Media = (a + b + c) / 3 # nao esqueça da ordem de precedencia
+
+print ("A média entre {:.1f} e {} será igual a {}".format(a, b, Media) # {:.1f} significa depois do ponto flutuante coloque apenas um digito
+
