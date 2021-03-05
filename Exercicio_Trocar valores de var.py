c1e998edf9e2cb8ec8724ba4efdd7677c6838a50
DiegoSkieresz <46655332+SkiereszDiego@users.noreply.github.com>
2021-03-05 19:39:21 -0300

    Outra versao para resposta

--- Exercicio_Trocar valores de var.py
+++ Exercicio_Trocar valores de var.py
@@ -6,15 +6,19 @@
     troca e após a troca.
 '''
 
+
 vA = int(input("digite um numero:"))
 vB = int(input("digite outro numero:"))
 vC = 0
 
-print ("Antes da troca o","vA era ",vA,"e o vB era ", vB)
-
+print ('Antes da troca o VA era {} e o vB era {}'.format(vA,vB))
 vC = vA # A gente guarda o valor de vA para não se perder
 vA = vB # Coloca o valor de vB no vA
 vB = vC # Pega o valor que era do vA de dentro do vC
+print ('Depois da troca o VA sera {} e o vB sera {}'.format(vA,vB))
 
-
-print ("Depois da troca o","vA fica ",vA,"e o vB sera ", vB)
+########################ou##########################################
+va = float(input('digite vA: '))
+vb = float(input('digite vB: '))
+va , vb = vb , va
+print('va era {} e vb era {} agora va é {} e vb é {}' .format(vb,va,va,vb))
